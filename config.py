d6ce62b9164de3a0191311dabd1229ead202b51c
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-03-28 07:06:25 -0400

    modify DATABASE_URL

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 REFRESH_TOKEN_URL = "https://api.princeton.edu:443/token"
 
-DATABASE_URL = environ['DATABASE_URL'][:8]+'ql'+environ['DATABASE_URL'][8:]
+DATABASE_URL = environ['DATABASE_URL']
