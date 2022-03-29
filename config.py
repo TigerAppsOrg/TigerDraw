da2a4bce314dcc5db7e6f8b4182d065afa9d6f98
Adam Gamba <agamba@princeton.edu>
2022-03-29 16:29:02 -0400

    floor plan pdfs for yoseloff

--- config.py
+++ config.py
@@ -1,11 +1,23 @@
 from os import environ
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Keys for access to the Princeton API
-DIRECTORY_BASE_URL = "https://api.princeton.edu:443/active-directory/1.0.3"
+DIRECTORY_BASE_URL = (
+    "https://api.princeton.edu:443/active-directory/1.0.3"
+)
 USERS = "/users/basic"
 ACCESS_TOKEN = ""
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
+
+
+# print("*test", environ["HOME"])
+CONSUMER_KEY = environ["CONSUMER_KEY"]
+# b"\xe7\xa3\x1c\xf2\n\xe9\xb6\xd0\xcd\xbcI\xa9\x14\x8a\x91\x86"
+
+CONSUMER_SECRET = b"\xe7\xa3\x1c\xf2\n\xe9\xb6\xd0\xcd\xbcI\xa9\x14\x8a\x91\x86"  # environ["CONSUMER_SECRET"]
 REFRESH_TOKEN_URL = "https://api.princeton.edu:443/token"
 
-DATABASE_URL = environ['DATABASE_URL'][:8]+'ql'+environ['DATABASE_URL'][8:]
+DATABASE_URL = (
+    environ["DATABASE_URL"][:8] + "ql" + environ["DATABASE_URL"][8:]
+)
