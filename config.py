8bfb2a1c9e013d141cceec565df35b133daf48b5
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-03-30 17:46:12 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,18 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Keys for access to the Princeton API
-DIRECTORY_BASE_URL = (
-    "https://api.princeton.edu:443/active-directory/1.0.3"
-)
+DIRECTORY_BASE_URL = "https://api.princeton.edu:443/active-directory/1.0.3"
 USERS = "/users/basic"
 ACCESS_TOKEN = ""
 
 
-# print("*test", environ["HOME"])
 CONSUMER_KEY = environ["CONSUMER_KEY"]
-# b"\xe7\xa3\x1c\xf2\n\xe9\xb6\xd0\xcd\xbcI\xa9\x14\x8a\x91\x86"
-
-CONSUMER_SECRET = b"\xe7\xa3\x1c\xf2\n\xe9\xb6\xd0\xcd\xbcI\xa9\x14\x8a\x91\x86"  # environ["CONSUMER_SECRET"]
+CONSUMER_SECRET = environ["CONSUMER_SECRET"]
 REFRESH_TOKEN_URL = "https://api.princeton.edu:443/token"
 
 DATABASE_URL = environ["DATABASE_URL"]
