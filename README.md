69919a64f0f1d5cd972d540b864f19ecb52d7398
Danielpark1239 <Danielpark1239@gmail.com>
2024-01-28 20:34:29 -0500

    set up local development

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TigerDraw
+
+# Local Development
+Install dependencies:
+- `python -m venv venv`
+- `./venv/bin/activate` for Mac and `venv\Scripts\activate` for Windows
+- `pip install -r requirements.txt`
+
+Env Variables: fill in .env.template and rename to .env, then run `python wsgi.py`
--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from os import environ
+from dotenv import load_dotenv
 
 # Keys for access to the Princeton API
 DIRECTORY_BASE_URL = "https://api.princeton.edu:443/active-directory/1.0.3"
 USERS = "/users/basic"
 ACCESS_TOKEN = ""
 
-
 CONSUMER_KEY = environ["CONSUMER_KEY"]
 CONSUMER_SECRET = environ["CONSUMER_SECRET"]
 REFRESH_TOKEN_URL = "https://api.princeton.edu:443/token"
 
 DATABASE_URL = environ["DATABASE_URL"]
+
+load_dotenv()
